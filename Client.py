d2ec5c4937055ae16d4b804c512a83ce1d26f033
Alexander <AlexKnows@users.noreply.github.com>
2016-10-28 09:41:47 +0000

    Update Client.py

--- Client.py
+++ Client.py
@@ -9,23 +9,31 @@ MEDIUM_FONT = ("Verdana", 10)
 class ClientApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self)
 
-        container.pack(side="top", fill="both", expand=True)
+        self.container = tk.Frame(self)
 
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        self.container.pack(side="top", fill="both", expand=True)
+
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
-        for F in (StartPage, PageOne):
-            frame = F(container, self)
-            self.frames[F] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
+        frame = StartPage(self.container, self)
+        self.frames[StartPage] = frame
+        frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+    def show_user_frame(self, cont, ip, port):
+        frame = Game(self.container, self, ip, port)
+        self.frames[Game] = frame
+        frame.grid(row=0, column=0, sticky="nsew")
+
         frame = self.frames[cont]
         frame.tkraise()
 
@@ -48,23 +56,34 @@ class StartPage(tk.Frame):
 
     def connect_client(self, server_address):
         ip, port = server_address.split(':')
-        s = socket.socket()
-        try:
-            s.connect((ip, int(port)))
-            self.controller.show_frame(PageOne)
-        except socket.error as error:
-            error_label = tk.Label(self, text=error, font=MEDIUM_FONT, fg="red")
-            error_label.pack()
+
+        self.controller.show_user_frame(Game, ip, port)
 
 
-class PageOne(tk.Frame):
-    def __init__(self, parent, controller):
+class Game(tk.Frame):
+    def __init__(self, parent, controller, ip, port):
+        self.ip = ip
+        self.port = port
+        self.controller = controller
+        self.s = socket.socket()
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Connected to server ", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
+        
+        if self.connect_client():
+            label = tk.Label(self, text="Connected to server ", font=LARGE_FONT)
+            label.pack(pady=10, padx=10)
 
         button1 = ttk.Button(self, text="Back to home", command=lambda: controller.show_frame(StartPage))
         button1.pack()
+
+    def connect_client(self):
+        try:
+            self.s.connect((self.ip, int(self.port)))
+            self.controller.show_user_frame(Game)
+            return True
+        except socket.error as error:
+            error_label = tk.Label(self, text=error, font=MEDIUM_FONT, fg="red")
+            error_label.pack()
+            return False
 
 app = ClientApp()
 
